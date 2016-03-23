60fc693461f994a476b2cdd234bed93a4999fbff
Henri Carnot <henri.carnot@hotmail.fr>
2016-03-23 15:15:00 +0100

    Added the project

---
+++ Client/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("192.168.0.14", 2055);
+
+            client.ReceiveTimeout = 500;
+            NetworkStream s = client.GetStream();
+
+            StreamReader sr = new StreamReader(s);
+
+            StreamWriter sw = new StreamWriter(s);
+            sw.AutoFlush = true;
+            client.NoDelay = true;
+            s.ReadTimeout = 1000;
+
+            Console.WriteLine(client.Available.ToString());
+            Console.WriteLine(s.CanRead.ToString());
+            // find regex
+            string pattern = @"^<(?:[^\>]*)+>";
+            string pattern_in = @"(?:[^\<\/\>]+)";
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            Regex rgx_in = new Regex(pattern_in, RegexOptions.IgnoreCase);
+            // initialisation = ok
+            while (true)
+            {
+
+                //string input_text = sr.ReadLine();
+                sw.Flush();
+
+                try
+                {
+
+                    string inputText = sr.ReadLine();
+                    MatchCollection matches = rgx.Matches(inputText);
+                    if (matches.Count > 0)
+                    {
+
+                        MatchCollection matches_in = rgx_in.Matches(inputText);
+                        string parameterName = matches_in[0].Value;
+
+                        if (parameterName == "break")
+                        {
+                            break;
+                        }
+                        if (parameterName == "input")
+                        {
+                            Console.Write("input > ");
+                            sw.WriteLine(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.Write(parameterName);
+                            sw.WriteLine(Console.ReadLine());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(inputText);
+                    }
+
+
+
+                }
+                catch (IOException)
+                {
+                    //sw.WriteLine(Console.ReadLine());
+                }
+
+
+
+                /*sw.WriteLine(Console.ReadLine());
+                Console.WriteLine(sr.ReadLine());*/
+            }
+
+            s.Close();
+            try
+            { }
+            finally
+            {
+                Console.WriteLine("client closed");
+                client.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace Server
+{
+    class Program
+    {
+        static string input = "";
+
+        static TcpListener listener;
+        const int LIMIT = 5; //5 concurrent clients
+
+        static void Main(string[] args)
+        {
+            string localComputerName = Dns.GetHostName();
+            Console.WriteLine("DNS : " + localComputerName);
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress input in localIPs)
+            {
+                Console.WriteLine(input.ToString());
+            }
+            listener = TcpListener.Create(2055);
+            listener.Start();
+
+            Console.WriteLine("Server mounted, listening to port 2055");
+
+            for (int i = 0; i < LIMIT; i++)
+            {
+                Thread t = new Thread(new ThreadStart(Service));
+                t.Start();
+            }
+        }
+        public static void Service()
+        {
+
+            string DirectoryPath = @"D:\";
+            while (true)
+            {
+                Console.Title = "Centrall";
+                Socket soc = listener.AcceptSocket();
+                Console.WriteLine("Connected: {0}", soc.RemoteEndPoint);
+                try
+                {
+                    NetworkStream s = new NetworkStream(soc);
+                    StreamReader sr = new StreamReader(s, Encoding.UTF8);
+                    StreamWriter sw = new StreamWriter(s, Encoding.UTF8);
+                    sw.AutoFlush = true; // enable automatic flushing
+
+
+                    // start
+                    string input;
+                    sw.WriteLine("=====================");
+                    sw.WriteLine("Hello in centrall!!!");
+                    sw.WriteLine("Writed by Henri2h in 2016");
+                    sw.WriteLine("=====================");
+                    sw.WriteLine();
+                    bool boocle = true;
+                    while (boocle)
+                    {
+                        sw.WriteLine("<input/>");
+                        input = sr.ReadLine();
+
+                        if (input == "time") { sw.WriteLine(System.DateTime.Now); }
+                        else if (input == "cls") { sw.WriteLine("This is sence to be cleared"); }
+                        else if (input == "dir")
+                        {
+                            sw.WriteLine("");
+                            sw.WriteLine("Ouverture du repertoire ....");
+                            sw.WriteLine("Repertoire : D:\\");
+                            sw.WriteLine("");
+
+                            foreach (string file in Directory.EnumerateFileSystemEntries(DirectoryPath))
+                            {
+
+
+                                string isDirectory = "";
+                                FileAttributes attr = File.GetAttributes(file);
+                                if (attr.HasFlag(FileAttributes.Directory))
+                                    isDirectory = "<dir>";
+                                else
+                                    isDirectory = "     ";
+                                string outputStringFile = File.GetLastAccessTime(file) + "   " + isDirectory + "   " + Path.GetFileName(file);
+                                sw.WriteLine(outputStringFile);
+                            }
+                            sw.WriteLine("");
+                        }
+                        else if (input == "cd")
+                        {
+                            sw.WriteLine("<Please enter direcory : />");
+                            string inputDirectory = sr.ReadLine();
+                            if (inputDirectory == "..")
+                            {
+                                try
+                                {
+                                    DirectoryPath = Directory.GetParent(DirectoryPath).ToString();
+                                }
+                                catch
+                                {
+                                    sw.WriteLine("cannot go out");
+                                }
+                            }
+                            else
+                            {
+                                string preDirectoryPath = retrunPath(DirectoryPath, inputDirectory);
+                                if (Directory.Exists(preDirectoryPath))
+                                {
+                                    DirectoryPath = preDirectoryPath;
+                                }
+                                else
+                                {
+                                    sw.WriteLine("This is not a directory");
+
+                                }
+                            }
+                        }
+                        else if (input == "type")
+                        {
+                            sw.WriteLine("<file : />");
+                            string inputDirectory = retrunPath(DirectoryPath, sr.ReadLine());
+                            sw.WriteLine(inputDirectory);
+                            if (File.Exists(inputDirectory))
+                            {
+                                sw.WriteLine(File.ReadAllText(inputDirectory));
+                            }
+                            else
+                            {
+                                sw.WriteLine("This file doesn't exist");
+                            }
+
+                        }
+                        else if (input == "chat")
+                        {
+                            while (true)
+                            {
+                                input = Console.ReadLine();
+                                sw.WriteLine(input);
+
+                            }
+
+                        }
+                        else if (input == "xml")
+                        {
+                            string xmlFileName = "D:\\Input.xml";
+                            XmlDocument xmlDoc = new XmlDocument();
+                            if (File.Exists(xmlFileName) == true)
+                            {
+                                xmlDoc.Load(xmlFileName);
+                            }
+                            else
+                            {
+                                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", null);
+                                xmlDoc.AppendChild(xmlDeclaration);
+                            }
+
+
+                            XmlElement xmlRoot = xmlDoc.DocumentElement;
+                            XmlNode newNode = xmlDoc.CreateNode(XmlNodeType.Element, "Voc", "");
+                            XmlNode nameNode = xmlDoc.CreateNode(XmlNodeType.Element, "Name", "");
+                            sw.WriteLine("#Name");
+                            nameNode.InnerText = sr.ReadLine();
+                            XmlNode versionNode = xmlDoc.CreateNode(XmlNodeType.Element, "Forname", "");
+                            sw.WriteLine("#Forname");
+                            versionNode.InnerText = sr.ReadLine();
+                            newNode.AppendChild(nameNode);
+                            newNode.AppendChild(versionNode);
+                            xmlRoot.AppendChild(newNode);
+
+                            xmlDoc.Save(xmlFileName);
+
+                        }
+                        else if (input == "xml node")
+                        {
+                            string xmlFileName = "D:\\Input.xml";
+                            XmlDocument xmlDoc = new XmlDocument();
+                            if (File.Exists(xmlFileName) == true)
+                            {
+                                xmlDoc.Load(xmlFileName);
+                                XmlNodeList nodeList;
+                                XmlNode root = xmlDoc.DocumentElement;
+                                nodeList = root.SelectNodes("//Voc");
+                                foreach (XmlNode xmlNode in nodeList)
+                                {
+                                    XmlNode Name = xmlNode.SelectSingleNode("Name");
+                                    XmlNode Forname = xmlNode.SelectSingleNode("Forname");
+                                    sw.WriteLine("Name : " + Name.InnerText + " Forname : " + Forname.InnerText);
+                                }
+                            }
+                        }
+                        else if (input == "exit")
+                        {
+                            sw.WriteLine("<break/>");
+                            break;
+                        }
+                        else { sw.WriteLine("please enter a valid input"); }
+                    }
+                    s.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine("Disconnected: {0}", soc.RemoteEndPoint);
+                soc.Close();
+            }
+        }
+
+
+        private static string retrunPath(string DirectoryPath, string inputDirectory)
+        {
+            if (DirectoryPath == Directory.GetDirectoryRoot(DirectoryPath))
+            {
+                return DirectoryPath + inputDirectory;
+            }
+            else
+            {
+                return DirectoryPath + "\\" + inputDirectory;
+            }
+        }
+        static void exit()
+        {
+            Console.Title = "goodbye";
+            Console.WriteLine("Press a key to exit");
+        }
+
+    }
+}
